b6aa032d897258c24d60ab0bfcde5a58cac2edcf
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-07-15 16:56:18 -0300

    create lesson 08 (work)

---
+++ B fase I/6_Lógica de Programação e Algoritmos/Aula08_Trabalho/questao02.py
@@ -0,0 +1,71 @@
+# -------------------------------- APRESENTAÇÃO E CARDÁPIO ------------------------------------
+print('Bem-vindo a Sorveteria do Carlos Henrique Monnerat Quintanilha')
+print('--------------------------------------Cardápio--------------------------------------')
+print('| Nº DE BOLAS  | Sabor Tradicional (tr) | Sabor Premium (pr) | Sabor Especial (es) |')
+print('|      1       |         R$ 6,00        |      R$ 7,00       |       R$ 8,00       |')
+print('|      2       |         R$ 11,00       |      R$ 13,00      |       R$ 15,00      |')
+print('|      3       |         R$ 15,00       |      R$ 18,00      |       R$ 21,00      |')
+print('------------------------------------------------------------------------------------')
+
+# -------------------------------- VARIÁVEL GLOBAL ------------------------------------
+valor = 0     # Acumulador para apresentar o valor total no final do programa
+
+# --------------------------- LOOPING E VALIDAÇÃO DAS ENTRADAS ------------------------------------
+while True:   # Cria um loop de execução das instruções.
+    sabor = input('Entre com o sabor desejado (tr/es/pr): ')
+# Verifica se o sabor digitado é válido ou não:
+    if(sabor == 'tr'):     # Caso o usuário digitar "tr" o código executará as instruções abaixo:
+        bolas = input('Entre com o número de bolas de sorvete desejado (1/2/3): ')
+        if(bolas == '1'):                           # Verifica se o número de bolas de sorvete digitado é válido ou não
+            valor += 6
+            print('Você pediu 1 bola de sorvete no sabor TRADICIONAL: R$ 6,00')
+        elif(bolas == '2'):
+            valor += 11
+            print('Você pediu 2 bolas de sorvete no sabor TRADICIONAL: R$ 11,00')
+        elif(bolas == '3'):
+            valor += 15
+            print('Você pediu 3 bolas de sorvete no sabor TRADICIONAL: R$ 15,00')
+        else:                                       # Caso o nº de bolas de sorvete for inválido, irá printar a msg a baixo:
+            print('Número de bolas de sorvete inválido. Tente novamente')
+            continue                                # O programa voltará para o início do looping.
+
+    elif(sabor == 'pr'):   # Caso o usuário digitar "pr" o código executará as instruções abaixo:
+        bolas = input('Entre com o número de bolas de sorvete desejado (1/2/3): ')
+        if (bolas == '1'):
+            valor += 7
+            print('Você pediu 1 bola de sorvete no sabor PREMIUM: R$ 7,00')
+        elif (bolas == '2'):
+            valor += 13
+            print('Você pediu 2 bolas de sorvete no sabor PREMIUM: R$ 13,00')
+        elif (bolas == '3'):
+            valor += 18
+            print('Você pediu 3 bolas de sorvete no sabor PREMIUM: R$ 18,00')
+        else:
+            print('Número de bolas de sorvete inválido. Tente novamente')
+            continue
+
+    elif (sabor == 'es'):  # Caso o usuário digitar "es" o código executará as instruções abaixo:
+        bolas = input('Entre com o número de bolas de sorvete desejado (1/2/3): ')
+        if (bolas == '1'):
+            valor += 8
+            print('Você pediu 1 bola de sorvete no sabor ESPECIAL: R$ 8,00')
+        elif (bolas == '2'):
+            valor += 15
+            print('Você pediu 2 bolas de sorvete no sabor ESPECIAL: R$ 15,00')
+        elif (bolas == '3'):
+            valor += 21
+            print('Você pediu 3 bolas de sorvete no sabor ESPECIAL: R$ 21,00')
+        else:
+            print('Número de bolas de sorvete inválido. Tente novamente')
+            continue
+    else:
+        print('Sabor inválido. Tente novamente')
+        continue
+# Verifica se o usuário deseja comprar mais alguma coisa:
+    add = input('Deseja mais algum sorvete (s/ digite outra tecla)?:  ')
+    if (add == 's'):          # Caso positivo, o programa retorna para o inicio do looping
+        continue
+    else:                     # Caso negativo, o programa imprime na tela o valor total a ser pego e encerra o programa.
+        print('-----------------------------------------------')
+        print('Valor total a ser pago: R${:.2f}'.format(valor))
+    break
---
+++ B fase I/6_Lógica de Programação e Algoritmos/Aula08_Trabalho/questao03.py
@@ -0,0 +1,93 @@
+# -------------------------- APRESENTAÇÃO ----------------------------
+print('Bem-vindo ao PetShop do Carlos Henrique Monnerat Quintanilha')
+
+# -------------------------- FUNÇÕES ---------------------------------
+def cachorro_peso(pergunta):    # Verifica o peso do cachorro e retorna a base
+    while True:
+        try:                    # Verifica se o peso digitado é válido ou não
+            peso = int(input(pergunta))
+            if(peso > 0 and peso < 3):
+                base = 40
+                break
+            elif(peso >= 3 and peso < 10):
+                base = 50
+                break
+            elif(peso >= 10 and peso < 30):
+                base = 60
+                break
+            elif(peso >= 30 and peso < 50):
+                base = 70
+                break
+            elif(peso <= 0):
+                print('-------------------------------------------------------------------')
+                print('Peso inválido.\nPor favor tente novamente.')
+                continue
+            elif (peso >= 50):
+                print('-------------------------------------------------------------------')
+                print('Não aceitamos cachorros tão grandes.\nPor favor entre com o peso do cachorro novamente.')
+                continue
+        except:                 # Caso o peso não seja válido, o programa irá imprimir a msg abaixo:
+            print('-------------------------------------------------------------------')
+            print('Você digitou um valor não numérico.\nPor favor entre com o peso do cachorro novamente.')
+            continue
+    return base
+
+def cachorro_pelo(pergunta):        # Verifica o pelo do cachorro e retorna o multiplicador
+    while True:
+        print('Entre com o PELO do cachorro: ')
+        print('c - Pelo Curto')
+        print('m - Pelo Médio')
+        print('l - Pelo Longo')
+        pelo = input(pergunta)
+        if(pelo != 'c' and pelo != 'm' and pelo != 'l'):
+            print('----------------------------------')
+            print('Pelo inválido. Tente novamente.')
+            continue
+        elif(pelo == 'c'):
+            multiplicador = 1
+            break
+        elif (pelo == 'm'):
+            multiplicador = 1.5
+            break
+        elif (pelo == 'l'):
+            multiplicador = 2
+            break
+    return multiplicador
+
+def cachorro_extra(pergunta):       # Verifica se o cliente deseja contratar um serviço adicional e retorna o valor extra
+    extra = 0
+    while True:
+        print('Deseja adicionar mais algum serviço?')
+        print('1 - Corte de Unhas - R$10,00')
+        print('2 - Escovar Dentes - R$12,00')
+        print('3 - Limpeza de Orelhas - R$15,00')
+        print('0 - Não desejo mais nada')
+        add = input(pergunta)
+        if(add != '1' and add != '2' and add != '3' and add != '0'):
+            print('-------------------------------------')
+            print('Adicional inválido. Por favor tente novamente.')
+            continue
+        elif(add == '1'):
+            extra += 10
+            continue
+        elif (add == '2'):
+            extra += 12
+            continue
+        elif (add == '3'):
+            extra += 15
+            continue
+        elif (add == '0'):
+            extra += 0
+            break
+    return extra
+
+# ----------------------------- PROGRAMA PRINCIPAL ----------------------------
+
+base = cachorro_peso('Entre com o PESO do cachorro: ')  # Chama a função 'cachorro_peso().'
+multiplicador = cachorro_pelo('>> ')                    # Chama a função 'cachorro_pelo().'
+extra = cachorro_extra('>> ')                           # Chama a função 'cachorro_extra().'
+
+total = base * multiplicador + extra                    # Calcula o total a ser pago.
+
+print('-------------------------------------')
+print('Total a pagar: R${:.2f}'.format(total))          # Imprime na tela o valor a ser pago.
